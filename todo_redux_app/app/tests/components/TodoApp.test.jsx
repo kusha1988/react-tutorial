e65d8e9189ce57b31b2c1caeceff238bec6a4edb
Vitalii Kushnir <vk@tenios.de>
2016-11-11 19:49:25 +0100

    Added React Redux Todo App

---
+++ todo_redux_app/app/tests/components/TodoApp.test.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
+var { Provider } = require('react-redux');
+var configureStore = require('configureStore');
+import TodoList from 'TodoList';
+
+var TodoApp = require('TodoApp');
+
+describe('TodoApp', () => {
+    it('should exist', () => {
+        expect(TodoApp).toExist();
+    });
+
+    it('should render TodoList', () => {
+        var store = configureStore.configure();
+        var provider = TestUtils.renderIntoDocument(
+            <Provider store={store}>
+                <TodoApp/>
+            </Provider>
+        );
+
+        var todoApp = TestUtils.scryRenderedComponentsWithType(provider, TodoApp)[0];
+        var todoList = TestUtils.scryRenderedComponentsWithType(todoApp, TodoList);
+
+        expect(todoList.length).toBe(1);
+    });
+
+});
